bf1f04d7f73632f30a6c455926992514d250007d
Hakkinen Wylgner <120416002+HakkinenWylgner@users.noreply.github.com>
2023-02-22 23:09:54 -0300

    Atualização de Desafio

--- app/src/main/java/com/example/desafioandroidjava/MainActivity.java
+++ app/src/main/java/com/example/desafioandroidjava/MainActivity.java
@@ -10,8 +10,11 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private TextInputEditText editPrecoAlcool, editPrecoGasolina;
     private TextView textResultado;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +24,46 @@ public class MainActivity extends AppCompatActivity {
         editPrecoAlcool    = findViewById(R.id.editPrecoAlcool);
         editPrecoGasolina  = findViewById(R.id.editPrecoGasolina);
         textResultado      = findViewById(R.id.textResultado);
+
+
     }
 
     public void calcularPreco(View view){
 
+        //recuperar valores digitas
+        String precoAlcool = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+
+        //Validar os campos digitados
+        Boolean camposValidados = validarCampos(precoAlcool, precoGasolina);
+        if ( camposValidados){
+
+            //Convertendo string para números
+            Double valorAlcool = Double.parseDouble(precoAlcool);
+            Double valorGasolina = Double.parseDouble(precoGasolina);
+            String texto;
+            if (valorAlcool / valorGasolina >= 0.7) {
+                texto = "É melhor utilizar gasolina";
+        /* Fazer cálculo de menor preço
+        Se (valorAlcool / valorGasolina) >= 0.7 é melhor utilizar gasolina
+        se não é melhor utilizar álcool
+        */
+            }else {
+                texto = "É melhor utilizar álcool";
+            }
+
+        }
+
+    }
+    public Boolean validarCampos ( String pAlcool, String pGasolina) {
+        Boolean camposValidados = true;
+        if (pAlcool == null || pAlcool.equals("")) {
+            camposValidados = false;
+        } else if (pGasolina == null || pGasolina.equals("")) {
+            camposValidados = false;
+        }
+
+        return camposValidados;
     }
 
 
